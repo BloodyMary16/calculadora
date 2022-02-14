0a73d64bf8d567dee2abc2d4a8b8f0e29f9892a2
María <maria_am16@hotmail.com>
2022-02-14 12:34:04 +0100

    ok

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿//Método sumar, restar, multiplicar y dividir. El resultado lo va a escribir por consola.
 //en program.cs hacer un menú. Escribir en la consola: introducir una opción. Repetir las opciones hasta que el usuario pulse "salir"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculadora
+{
+    public class Principal
+    {
+        public static void Main(string[] args)
+        {
+            public void Menu();
+
+
+
+        }
+
+    }
+}
