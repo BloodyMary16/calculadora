517cfcdadc1cabacc2318dc45e4e137e96769c1b
María <maria_am16@hotmail.com>
2022-02-24 09:33:40 +0100

    :)

--- Principal.cs
+++ Principal.cs
@@ -26,25 +26,20 @@ using System.Threading.Tasks;
             Console.WriteLine("5. Salir");
             Console.WriteLine("Opción:");
             opcion = Int32.Parse(Console.ReadLine());
-           
-        switch (opcion)
-        {
-            case 1:
-                int numero1;
-                int numero2;
-                Console.WriteLine("Introduzca un número: ");
-                numero1 = (Int32.Parse(Console.ReadLine()));
-                Console.WriteLine("Introduzca otro número: ");
-                numero2 = (Int32.Parse(Console.ReadLine()));
-                Calculadora.Suma();
-                break;
-           
+            try
+            {
+                opcion = Int32.Parse(Console.ReadLine());
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Debe introducir un número para seleccionar una opción.");
+                
+            }
 
         }
 
-        }
 
-       
         
 
 
--- calculadora.cs
+++ calculadora.cs
@@ -1,34 +1,38 @@
-public class Calculadora
-{
 
-    public static void Suma()
-    {
-
-        int result;
-        int num1 = 0;
-        int num2 = 0;  
-        result = num1 + num2;
-        Console.WriteLine("El resultado de " + num1 + " + " + num2 + "es: " + result);
-        Console.WriteLine("");
-        Console.WriteLine("Muchas gracias por utilizar nuestra aplicacion, \nesperamos volverte a ver muy pronto.");
-        Console.ReadLine();
-        Environment.Exit(0);
-    }
-    public void Resta()
-    {
-        int num1;
-        int num2;
-        int result;
-        Console.WriteLine("Introduzca un numero: ");
-        num1 = Int32.Parse(Console.ReadLine());
-        Console.WriteLine("Introduzca otro numero para proceder con la RESTA: ");
-        num2 = Int32.Parse(Console.ReadLine());
-        result = num1 - num2;
-        Console.WriteLine("El resultado de " + num1 + "- " + num2 + "es: " + result);
-        Console.WriteLine("");
-        Console.WriteLine("Muchas gracias por utilizar nuestra aplicacion, \nesperamos volverte a ver muy pronto.");
-        Console.ReadLine();
-        Environment.Exit(0);
+    public class Calculadora
+    { 
+       
+        public void Suma()
+        {
+            int num1;
+            int num2;
+            int result;
+            Console.WriteLine("Introduzca un numero: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca otro numero para proceder con la SUMA: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            result = num1 + num2;
+            Console.WriteLine("El resultado de " + num1 + "+ " + num2 + "es: " + result);
+            Console.WriteLine("");
+            Console.WriteLine("Muchas gracias por utilizar nuestra aplicacion, \nesperamos volverte a ver muy pronto.");
+            Console.ReadLine();
+            Environment.Exit(0);
+        }
+        public void Resta()
+        {
+            int num1;
+            int num2;
+            int result;
+            Console.WriteLine("Introduzca un numero: ");
+            num1 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Introduzca otro numero para proceder con la RESTA: ");
+            num2 = Int32.Parse(Console.ReadLine());
+            result = num1 - num2;
+            Console.WriteLine("El resultado de " + num1 + "- " + num2 + "es: " + result);
+            Console.WriteLine("");
+            Console.WriteLine("Muchas gracias por utilizar nuestra aplicacion, \nesperamos volverte a ver muy pronto.");
+            Console.ReadLine();
+            Environment.Exit(0);
 
     }
     public void Multiplicacion()
@@ -68,5 +72,6 @@ public class Calculadora
 
 
 
+    }
 }
 
